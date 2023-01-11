98364b7345fbc4628549e18571c2a62ca76feccd
anisulashik <anisulislamashik007@gmail.com>
2023-01-12 02:20:21 +0600

    TEST MESSAGE2

---
+++ contracts/hello.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract helloworld {
+    constructor() public{
+
+    }
+    function printf () public pure returns  (string memory){
+        return 'Anisul Islam Ashik';
+    }
+}
